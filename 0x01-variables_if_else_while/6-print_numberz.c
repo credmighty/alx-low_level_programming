920f20964aca750b23986e5cf6821a94405607b0
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-09 16:53:53 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - E
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
+}
