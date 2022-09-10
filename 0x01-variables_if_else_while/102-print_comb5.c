ff756fd765adcaa6f4b80e65259c1f6c12fd2ee0
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-09 21:28:35 -0700

    task 12- number combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - printing all possible combinations of two digits
+ *
+ * ranging from 0-99, separated by comma followed by a space
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int g1, g2;
+
+	for (g1 = 0; g1 <= 98; g1++)
+	{
+		for (g2 = g1 + 1; g2 <= 99; g2++)
+		{
+			putchar((g1 / 10) + '0');
+			putchar((g1 % 10) + '0');
+			putchar('\n');
+			putchar((g2 / 10) + '0');
+			putchar((g2 % 10) + '0');
+
+			if (g1 == 98 && g2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
