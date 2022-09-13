a2a2d9fc79652cf7a76c44aa39784b669b4ba474
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-13 16:17:01 -0700

    project 0x02 with header combo push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Desc: a fns to print alphabet
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	m = 0;
+
+	while (m < 10)
+	{
+		char l = 'a';
+
+		while (l <= 'z')
+		{
+			_putchar("%c", l);
+			l++;
+		}
+		_putchar("\n");
+		m++;
+	}
+}
