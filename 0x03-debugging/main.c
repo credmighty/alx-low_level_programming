8ab9c9d298c0a6b3fa9d6a66150895654567ee2c
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-15 12:24:15 -0700

    debugging task

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is +ve/-ve
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	positive_or_negative(c);
+	return (0);
+}
