fbe78feeed17b3aa1bd3c47095cfebe2dcd3c4b3
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-17 14:51:15 -0700

    printing straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,19 +3,16 @@
 /**
  * print_line - printing straight lines
  *
- * @n: number of times
+ * @n: number of time
  * Return: no return
  */
 void print_line(int n)
 {
-	int i, j;
+	int i;
 
 	if (n <= 0)
 		_putchar('\n');
 	for (i = 1; i <= n; i++)
-	{
-		for (j = 0; j < n - 1; j++)
-			_putchar('_');
-		_putchar('\n');
-	}
+		_putchar('_');
+	_putchar('\n');
 }
