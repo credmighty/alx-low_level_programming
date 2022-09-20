a5b3ca8b39de2a9f9f23bc74e582f11a5a4275e8
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-19 17:41:51 -0700

    fns to count string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - fns to count string
+ * @s: parameter to be passed
+ * Return: return value
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
