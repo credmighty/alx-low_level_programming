3bfe903ef958e411b1fe4451037f0d7aa96c1ffc
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-19 17:55:33 -0700

    This fns print string in reverse.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - fns to print a given string in reverse
+ * @s: parameter as be passed on the fns
+ * Return: reversed string
+ */
+void print_rev(char *s)
+{
+	int i, len = strlen(s);
+
+	for (i = len - 1; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
