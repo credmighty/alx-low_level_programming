be283331d317540ea639a5e6478e4b6ecf2e9b4d
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-20 17:09:53 -0700

    task 7 - printing diagonal line, task 8 - printing square

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,11 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n <= '0')
 		_putchar('\n');
-	for (i = 1; i <= n; i++)
+	for (i = '1'; i <= n; i++)
+	{
 		_putchar('_');
-	_putchar('$');
+	}
 	_putchar('\n');
 }
