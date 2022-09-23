0876b6d51dd42907c29867a175a66e34b3433d62
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-09-22 17:13:14 -0700

    magician: using pointer to change value.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- * desc: the code is to perform magic
- * Return: always 0
- */
 int main(void)
 {
 	int n;
@@ -13,7 +8,6 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
   /*
    * write your line of code here...
    * Remember:
@@ -22,8 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-
-	*(p + 5) = 98;
+	*(a + 2) = 98;
   /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
