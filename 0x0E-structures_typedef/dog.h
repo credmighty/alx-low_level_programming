605777d2e72fff94417b326dd62fd791625ef372
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-10-11 12:52:35 -0700

    new type struct dog with the following elements. Prototype for task 2,4 and 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 /**
  * struct dog - dog class
  * @name: Name of the dog
