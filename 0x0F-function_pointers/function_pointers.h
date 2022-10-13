a3305d162c9ca772f7415b75c81ac9a0a8ead3ea
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2022-10-13 11:35:39 -0700

    README file and header file rm stdlib corrected typo

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,5 @@
-#ifndef FUNCION_H
+#ifndef FUNCTION_H
 #define FUNCTION_H
-
-#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
