1824911d8ef2eb47fd6ddc4ded8032bd846a61f8
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2023-11-28 02:09:09 +0100

    pushing task 2 for appending-> func file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,8 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		len = 0;
-		while (text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
 	}
 	fp = open(filename, O_WRONLY | O_APPEND);
