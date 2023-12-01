825f7201858cecde8546f168a5eae89f84a30c4e
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2023-12-01 13:15:05 +0100

    lot of work for permission setting, read check created close func

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,8 +37,6 @@ void copy_file(const char *file_from, const char *file_to)
 		if (fw != fr)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_from);
-			close_file(from);
-			close_file(to);
 			exit(99);
 		}
 		if (fr == -1)
