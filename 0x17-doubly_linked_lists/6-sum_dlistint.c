e9c62f23d8245aad53260ef6cdc25cf087ff7d47
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-12 23:55:59 +0100

    sum of all... file 3,4 ade

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - Returns the sum of the data (n)
+ * @head: head arg for the list
+ * Return: sum of the list elements. 0 if no list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
