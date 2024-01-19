96453e1cc5d0394024880a27fdf5341c39d38665
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-19 23:52:04 +0100

    missed a letter b4 now code-obj-library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/credmighty/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/ligiga.so
+wget -P /tmp/ https://github.com/credmighty/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
