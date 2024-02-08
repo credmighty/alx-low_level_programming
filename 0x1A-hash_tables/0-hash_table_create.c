1cbfe51f9db6eb57eec340b5a7863672f189d42f
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-08 02:05:12 +0100

    0 betty styling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,16 +7,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *table = NULL;
 	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
 
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	table->size = size;
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
@@ -24,6 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
+	table->size = size;
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
