673e334a16ccfb0e63f257130e6ca44154961eeb
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-08 02:06:28 +0100

    0 betty styling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
