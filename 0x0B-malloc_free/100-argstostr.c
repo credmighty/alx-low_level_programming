b3a28d1cffae6304131f58cffcb52bf19d225ed0
Abdullahi Abdulrofiu <47549097+credmighty@users.noreply.github.com>
2025-03-18 00:13:32 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,34 +10,39 @@
 
 char *argstostr(int ac, char **av)
 {
-	char *new_str;
-	int len = 0, i = 0, j, k = 0;
+    char *str;
+    int i, j, pos, total_len = 0;
 
-	if (ac <= 0 || av == NULL)
-		return (NULL);
+    // Check for invalid input
+    if (ac == 0 || av == NULL) {
+        return NULL;
+    }
 
-	for (; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			len++;
-		len++;
-	}
+    // Calculate total length (including \n for each arg and final \0)
+    for (i = 0; i < ac; i++) {
+        total_len += strlen(av[i]) + 1;  // Length of arg + \n
+    }
+    total_len += 1;  // For the final null terminator
 
-	len++;
-	new_str = malloc(len * sizeof(char));
-	if (new_str == NULL)
-		return (NULL);
+    // Allocate memory for the new string
+    str = (char *)malloc(total_len * sizeof(char));
+    if (str == NULL) {
+        return NULL;
+    }
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			new_str[k] = av[i][j];
-			k++;
-		}
-		new_str[k] = '\n';
-		k++;
-	}
-	new_str[k] = '\0';
-	return (new_str);
+    // Build the concatenated string
+    pos = 0;
+    for (i = 0; i < ac; i++) {
+        // Copy the argument
+        for (j = 0; av[i][j] != '\0'; j++) {
+            str[pos] = av[i][j];
+            pos++;
+        }
+        // Add newline
+        str[pos] = '\n';
+        pos++;
+    }
+    str[pos] = '\0';  // Null-terminate the string
+
+    return str;
 }
