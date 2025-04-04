1383a448f5ea431f58e768099deb51e3f2221c74
Abdullahi Abdulrofiu <47549097+credmighty@users.noreply.github.com>
2025-04-04 11:14:20 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,37 +13,40 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *current_node = *head, *new_node_ptr;
+        listint_t *ptr, *new_node;
+        unsigned int pos = 0;
 
-	if (idx == 0)
-	{
-		new_node_ptr = malloc(sizeof(listint_t));
+        if (*head == NULL)
+                return (NULL);
 
-		if (!new_node_ptr)
-			return (NULL);
-		new_node_ptr->n = n;
-		new_node_ptr->next = *head;
-		*head = new_node_ptr;
-		return (new_node_ptr);
-	}
+        new_node = malloc(sizeof(listint_t));
+        if (new_node == NULL)
+                return (NULL);
 
-	while (current_node && ((i + 1) != idx))
-	{
-		current_node = current_node->next;
-		i++;
-	}
+        new_node->n = n;
+        new_node->next = NULL;
 
-	if ((i + 1) == idx)
-	{
-		new_node_ptr = malloc(sizeof(listint_t));
+        if (idx == 0)
+        {
+                new_node->next = *head;
+                *head = new_node;
+                return (new_node);
+        }
 
-		if (!new_node_ptr)
-			return (NULL);
-		new_node_ptr->n = n;
-		new_node_ptr->next = current_node->next;
-		current_node->next = new_node_ptr;
-		return (new_node_ptr);
-	}
-	return (NULL);
+        ptr = *head;
+
+        while (ptr != NULL && pos < idx - 1)
+        {
+                pos++;
+                ptr = ptr->next;
+        }
+        if (ptr == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
+        new_node->next = ptr->next;
+        ptr->next = new_node;
+
+        return (new_node);
 }
